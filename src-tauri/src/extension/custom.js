32700b8adf44d6d22b08c342337ec75b063013e3
sitemayerlee <sitemayerlee@163.com>
2025-02-23 21:19:11 +0800

    update custom js from pakeplus

--- src-tauri/src/extension/custom.js
+++ src-tauri/src/extension/custom.js
@@ -46,3 +46,25 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     observer.observe(targetNode, config)
 })
+
+// css filter
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const element0 = document.querySelector('xxx');
+                if (element0) {
+                    element0.style.display = 'none';
+                }
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+// end css filter
